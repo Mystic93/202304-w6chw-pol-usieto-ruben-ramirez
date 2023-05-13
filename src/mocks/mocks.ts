291ef850440c89a03654e995d04de455efefe36e
Pol <polgil24@hotmail.com>
2023-05-13 13:14:25 +0200

    Add robotSlice test

---
+++ src/mocks/mocks.ts
@@ -0,0 +1,28 @@
+import { RobotsDocumentStructure } from "../types";
+
+export const robotsMock: RobotsDocumentStructure[] = [
+  {
+    name: "r2d2",
+    creationDate: 2000,
+    resistance: 10,
+    speed: 8,
+    _id: "12234",
+    img: "",
+  },
+  {
+    name: "joseluis",
+    creationDate: 2001,
+    resistance: 7,
+    speed: 5,
+    _id: "12235",
+    img: "",
+  },
+  {
+    name: "ignacio",
+    creationDate: 2002,
+    resistance: 9,
+    speed: 3,
+    _id: "12236",
+    img: "",
+  },
+];
---
+++ src/store/robots/robotSlice.test.ts
@@ -0,0 +1,27 @@
+import { robotsMock } from "../../mocks/mocks";
+import { loadRobotsActionCreator, robotReducer } from "./robotsSlice";
+import { RobotStateStructure } from "./types";
+
+describe("Given loadRobot reducer", () => {
+  describe("When it receives three robots and an empty robot list", () => {
+    test("Then it shoul return a list with three robots", () => {
+      const robotsState: RobotStateStructure = {
+        robots: [],
+      };
+
+      const loadRobotsAction = loadRobotsActionCreator(robotsMock);
+
+      const expectedRobotList: RobotStateStructure = {
+        ...robotsState,
+        robots: robotsMock,
+      };
+
+      const newState: RobotStateStructure = robotReducer(
+        robotsState,
+        loadRobotsAction
+      );
+
+      expect(expectedRobotList).toStrictEqual(newState);
+    });
+  });
+});
