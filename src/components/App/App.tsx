3536c56aca4b7b135e58a1442cab958301d123de
Pol <polgil24@hotmail.com>
2023-05-13 18:53:21 +0200

    Apply custom hook in App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,22 @@
+import { useEffect } from "react";
+import useApi from "../../hooks/useApi";
+import { useAppDispatch } from "../../store";
+import { loadRobotsActionCreator } from "../../store/robots/robotsSlice";
+
 const App = (): JSX.Element => {
-  return <p>Hello world!</p>;
+  const { loadRobots } = useApi();
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    (async () => {
+      const initialRobots = await loadRobots();
+
+      dispatch(loadRobotsActionCreator(initialRobots));
+    })();
+  }, [dispatch, loadRobots]);
+
+  return <h1 className="title">¡Your Robots!</h1>;
 };
 
 export default App;
