a9eb95fdea0fffeb7a2e4028e022d5d80d5a4ee7
Pol <polgil24@hotmail.com>
2023-05-14 15:00:33 +0200

    Fix RobotList test

--- src/components/RobotList/RobotList.test.tsx
+++ src/components/RobotList/RobotList.test.tsx
@@ -5,25 +5,26 @@ import { robotsMock } from "../../mocks/mocks";
 
 describe("Given RobotList component", () => {
   describe("When it receives a list with r2d2,joseluis and ignacio", () => {
-    test("Then it shoul show r2d2, joseluis and ignacio cards", () => {});
-    const r2d2Name = "r2d2";
-    const joseluisName = "joseluis";
-    const ignacioName = "ignacio";
+    test("Then it shoul show r2d2, joseluis and ignacio cards", () => {
+      const r2d2Name = "r2d2";
+      const joseluisName = "joseluis";
+      const ignacioName = "ignacio";
 
-    render(<RobotList robots={robotsMock} />);
+      render(<RobotList robots={robotsMock} />);
 
-    const r2d2 = screen.getByRole("heading", {
-      name: r2d2Name,
-    });
-    const joseluis = screen.getByRole("heading", {
-      name: joseluisName,
-    });
-    const ignacio = screen.getByRole("heading", {
-      name: ignacioName,
-    });
+      const r2d2 = screen.getByRole("heading", {
+        name: r2d2Name,
+      });
+      const joseluis = screen.getByRole("heading", {
+        name: joseluisName,
+      });
+      const ignacio = screen.getByRole("heading", {
+        name: ignacioName,
+      });
 
-    expect(r2d2).toBeInTheDocument();
-    expect(joseluis).toBeInTheDocument();
-    expect(ignacio).toBeInTheDocument();
+      expect(r2d2).toBeInTheDocument();
+      expect(joseluis).toBeInTheDocument();
+      expect(ignacio).toBeInTheDocument();
+    });
   });
 });
